3d51abb3418b6b050633c25850aa7c42f38f0954
khuur <kristjan.bleiweis@gmail.com>
2019-12-25 23:26:09 +0100

    2 players can add tanks and they move at the same time in different dirrections

--- game.py
+++ game.py
@@ -33,9 +33,12 @@ background_image = pygame.image.load("./data/map.jpg").convert()
 user_interface = pygame.image.load("./data/user_interface1.png").convert()
 done = False
 
-player1 = units.Player('player1', screen)
-player2 = units.Player('player2', screen)
-all_static_objects.append(player1.addSoldier())
+players = [units.Player('player1', screen), units.Player('player2', screen)]
+
+which_player = 0
+soldier = players[0].addSoldier()
+if soldier:
+    all_static_objects.append(soldier)
 clock = pygame.time.Clock()
 
 while not done:  # main game loop
@@ -47,22 +50,46 @@ while not done:  # main game loop
     pressed = pygame.key.get_pressed()
 
     if pressed[pygame.K_UP]:
-        player1.army[0].move("up")
+        players[which_player].army[0].move("up")
     if pressed[pygame.K_DOWN]:
-        player1.army[0].move("down")
+        players[which_player].army[0].move("down")
     if pressed[pygame.K_LEFT]:
-        player1.army[0].move("left")
+        players[which_player].army[0].move("left")
     if pressed[pygame.K_RIGHT]:
-        player1.army[0].move("right")
+        players[which_player].army[0].move("right")
 
     if pressed[pygame.K_r]:
-        player1.addSoldier()
+        pass
+
+    if pressed[pygame.K_SPACE]:
+        which_player = 1 - which_player
 
     if pressed[pygame.K_t]:
         for object in all_static_objects:
             object.print()
 
-    if (mouse_clicks != pygame.mouse.get_pressed()):
+    if pygame.mouse.get_pressed()[0]:
+        #torej je lev prtisjen
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        print("x:", mouse_x)
+        print("y:", mouse_y)
+
+        if 658 < mouse_x < 785:
+            # pomeni, da pritiska nekje po tem UIju
+            if 20 < mouse_y < 65:
+                print("Players switched")
+                which_player = 1 - which_player
+            elif 100 < mouse_y < 222:
+                print("Mini map")
+            elif 235 < mouse_y < 285:
+                print("GOLD : ")
+            elif 317 < mouse_y < 373:
+                soldier = players[which_player].addSoldier()
+                if soldier:
+                    all_static_objects.append(soldier)
+
+    if mouse_clicks != pygame.mouse.get_pressed():
         # al je nekdo pritisnu na miško al pa spustu knof
         lev, sredn, desn = mouse_clicks
         x1, y1 = mouse_position
@@ -73,24 +100,29 @@ while not done:  # main game loop
 
         x2, y2 = mouse_position
 
-        if (lev + desn + sredn > sum(mouse_clicks)):
+        x = sorted([x1, x2])
+        y = sorted([y1, y2])
+
+        if lev + desn + sredn > sum(mouse_clicks):
 
             for object in all_static_objects:
-                if x1 < object.x < x2 and y1 < object.y < y2:
+                if x[0] < object.center_x < x[1] and y[0] < object.center_y < y[1]:
+                    # To pomeni, da je objekt znotraj recttangle-a, ki ga z miško obdaja
                     object.selected = 1
                     object.print()
-                    object.setDestination(object.x, object.y)
+                    #object.setDestination(object.x, object.y)
                 else:
                     # Zdej si dobu ukaz, da se premakn tja kamor miška zdej kaže
                     if sredn:
                         for selected_object in all_static_objects:
                             if selected_object.selected:
                                 selected_object.setDestination(mouse_position[0], mouse_position[1])
+                                selected_object.goTo()
                     else:
                         object.selected = 0
 
     for selected_object in all_static_objects:
-        if selected_object.selected:
+        if selected_object.distance > 10:
             selected_object.goTo()
 
     x, y = mouse_position
--- units.py
+++ units.py
@@ -4,21 +4,22 @@ from math import sqrt
 import units
 from functions import *
 import time
+from datetime import datetime
 
 
 class Unit:
     speed: float
 
     def __init__(self, screen, x, y, image_path, name):
-        self.x = x
+        self.x = x # kje na screenu sploh je
         self.y = y
-        self.speed = 0.4
-        self.direction_x = 0
+        self.speed = 1.5 # njegova hitrost
+        self.direction_x = 0 # kam more it
         self.direction_y = 0
-        self.selected = 0
-        self.moveable = 0
-        self.distance = 0
-        self.name = name
+        self.selected = 0 # a je ta unit selectan?
+        self.moveable = 0 # a se sploh lahko premika?
+        self.distance = 0 # Kok stran od cilja j e
+        self.name = name  # Kako je temu unitu bolj natančno ime
         self.screen = screen  # kam sploh rišeš
         self.image = pygame.image.load(image_path)  # prvi sprite
         self.image_selected = pygame.image.load(image_path)  # prvi sprite
@@ -26,16 +27,14 @@ class Unit:
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
-        self.center_x = self.x + (self.rect[2] // 2)
+        self.center_x = self.x + (self.rect[2] // 2) # Center tega unita
         self.center_y = self.y + (self.rect[3] // 2)
 
-        self.r = (self.rect[2] // 2 + self.rect[3] // 2) // 2
+        self.r = (self.rect[2] // 2 + self.rect[3] // 2) // 2 # Radij tega unita
 
-        # Set a variable for each movement.
-        self.moving_right = False
-        self.moving_left = False
-        self.moving_up = False
-        self.moving_down = False
+        self.max_hp = 30
+        self.hp = self.max_hp
+        self.attack = 5
 
     def print(self):
         print("Name: ", self.name)
@@ -46,44 +45,22 @@ class Unit:
         time.sleep(0.3)
         print("\n")
 
+    def updateCenter(self):
+        self.center_x = self.x + (self.rect[2] // 2)
+        self.center_y = self.y + (self.rect[3] // 2)
+
     def move(self, direction):
-        neki = True
-        if neki:
-            if direction == "up":
-                self.y -= self.speed
-            elif direction == "down":
-                self.y += self.speed
-            elif direction == "left":
-                self.x -= self.speed
-            elif direction == "right":
-                self.x += self.speed
-        else:
-            for object in all_static_objetcts:
-                if collisionDetection(self, object, False):
-                    print("fak")
-                    print(object.name)
-                    print(self.x)
-                    print(self.y)
 
-            self.x = 730
-            self.y = 730
+        if direction == "up":
+            self.y -= self.speed
+        elif direction == "down":
+            self.y += self.speed
+        elif direction == "left":
+            self.x -= self.speed
+        elif direction == "right":
+            self.x += self.speed
 
-    def blitme(self):
-        self.screen.blit(self.image, self.rect)
-
-    def update(self):
-
-        if self.rect.right <= self.screen_rect.right and self.moving_right:
-            self.rect.centerx += 3
-
-        if self.rect.left > 0 and self.moving_left:
-            self.rect.centerx -= 3
-
-        if self.rect.top > 0 and self.moving_up:
-            self.rect.bottom -= 3
-
-        if self.rect.bottom <= self.screen_rect.bottom and self.moving_down:
-            self.rect.bottom += 3
+        self.updateCenter()
 
     def changeImage(self, which_one, path):
         if which_one == "main":
@@ -92,33 +69,34 @@ class Unit:
             self.image_selected = getImage(path)
 
     def goTo(self):
+        """
+        Ta funkcija premakne ta unit tja kamor je namenjen (direction_x, y)
+        :return: razdaljo od cilja
+        """
+        self.distance = sqrt((self.direction_x - self.center_x) ** 2 + (self.direction_y - self.center_y) ** 2)
 
-        mouse_x = self.direction_x
-        mouse_y = self.direction_y
+        if self.distance < 10:
+            return self.distance
 
-        self.distance = sqrt((mouse_x - self.x) ** 2 + (mouse_y - self.y) ** 2)
-
-        if self.distance < 20:
-            return None
-
-
-        if self.x < mouse_x:
+        if self.center_x < self.direction_x:
             self.move("right")
         else:
             self.move("left")
 
-        if self.y > mouse_y:
+        if self.center_y > self.direction_y:
             self.move("up")
         else:
             self.move("down")
-			
-        self.distance = sqrt((mouse_x - self.x) ** 2 + (mouse_y - self.y) ** 2)
-        return sqrt((mouse_x - self.x) ** 2 + (mouse_y - self.y) ** 2)
+
+        self.distance = sqrt((self.direction_x - self.center_x) ** 2 + (self.direction_y - self.center_y) ** 2)
+        return self.distance
 
     def setDestination(self, dest_x, dest_y):
         self.direction_x = dest_x
         self.direction_y = dest_y
 
+    def setHp(self, hp):
+        self.hp = hp
 
 class Soldier(Unit):
     def __init__(self, screen, x, y, image_path, name):
@@ -126,53 +104,24 @@ class Soldier(Unit):
         super().__init__(screen, x, y, image_path, name)
         self.moveable = 1
 
+    def fight(self):
+        print("now i'm fighting")
+        print("HP : ", self.hp)
+        print("Attack : ", self.attack)
+
+    def levelUp(self):
+        self.max_hp = self.max_hp * 2
+        self.hp = self.max_hp
+
 
 class House(Unit):
 
     def __init__(self, screen, x, y, image_path, name):
         """Initialize the soldier and set its starting position."""
         super().__init__(screen, x, y, image_path, name)
+        self.setHp(500)
 
 
-class Corn(Unit):
-    def __init__(self, screen, x, y, image_path, name):
-        """Initialize the soldier and set its starting position."""
-        super().__init__(screen, x, y, image_path, name)
-
-
-class Pikca(Unit):
-
-    def __init__(self, screen, x, y, image_path, name):
-        """Initialize the soldier and set its starting position."""
-        super().__init__(screen, x, y, image_path, name)
-        self.moveable = 1
-
-    def move(self, direction):
-        neki = True
-        for object in all_static_objetcts:
-            if collisionDetection(self, object, False):
-                neki = False
-
-        if neki:
-            if direction == "up":
-                self.y -= 3
-            elif direction == "down":
-                self.y += 3
-            elif direction == "left":
-                self.x -= 3
-            elif direction == "right":
-                self.x += 3
-        else:
-            for object in all_static_objetcts:
-                if collisionDetection(self, object, False):
-                    print("fak")
-                    print(object.name)
-                    print(self.x)
-                    print(self.y)
-                    print(self.selected)
-
-            self.x = 730
-            self.y = 730
 
 class Player:
 
@@ -181,8 +130,16 @@ class Player:
         self.army = []
         self.buildings = []
         self.screen = screen
+        self.last_soldier_added = time.time()
 
     def addSoldier(self):
-        soldier = Soldier(self.screen, 300 + len(self.army) * 30, 300 + len(self.army) * 30, './data/' + str(self.player) +'/tank.png', 'Soldier' + str(len(self.army)))
-        self.army.append(soldier)
-        return soldier
+
+        if time.time() - self.last_soldier_added > 3:
+            soldier = Soldier(self.screen, 300 + len(self.army) * 30, 300 + len(self.army) * 30,
+                              './data/' + str(self.player) + '/tank.png', 'Soldier' + str(len(self.army)))
+            self.army.append(soldier)
+            self.last_soldier_added = time.time()
+            return soldier
+        return 0
+
+
